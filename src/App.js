b29a3839f5d3089e5034f1d4b6793320828f9c36
Aleksandra Manczyk <aleksandra.manczyk@sienn.pl>
2023-09-25 15:40:41 +0200

    Clean up code

--- src/App.js
+++ src/App.js
@@ -5,20 +5,18 @@ import { useState } from 'react';
 
 function App() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const [isXNext, setisXNext] = useState(true);
   const [currentMove, setCurrentMove] = useState(0);
+  const isXNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
   const handleOnPlay = (newSquares) => {
     const nextHistory = [...history.slice(0, currentMove + 1), newSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
-    setisXNext(!isXNext);
   };
 
   const goToMove = (currentMove) => {
     setCurrentMove(currentMove);
-    setisXNext(currentMove % 2 === 0);
   };
 
   const gameHistory = history.map((squares, index) => {
